41819e5e30ad0eb26441a1b6c4febeb9ee3fbfcc
RealCatDev <real.catdev@gmail.com>
2024-02-12 23:41:14 +0100

    Rendering DLC

---
+++ core/include/PurrfectEngine/camera.hpp
@@ -0,0 +1,39 @@
+#ifndef PURRENGINE_CAMERA_HPP_
+#define PURRENGINE_CAMERA_HPP_
+
+#include "PurrfectEngine/transform.hpp"
+
+namespace PurrfectEngine {
+
+  struct CameraUBO {
+    glm::mat4 proj;
+    glm::mat4 view;
+  };
+
+  class purrCamera {
+  public:
+    purrCamera(purrTransform *trans):
+      mTransform(trans)
+    {}
+
+    ~purrCamera() {
+      delete mTransform;
+    }
+
+    void calculate(glm::ivec2 size) {
+      mProj = glm::perspective(glm::radians(mFOV), size.x / (float) size.y, mNearPlane, mFarPlane);
+      mProj[1][1] *= -1;
+      mView = glm::lookAt(mTransform->getPosition(), mTransform->getPosition() + mTransform->getForward(), glm::vec3(0.0f, 1.0f, 0.0f));
+    }
+
+    glm::mat4 getProjection() const { return mProj;   }
+    glm::mat4 getView()       const { return mView;   }
+  private:
+    float mFOV = 45.0f, mNearPlane = 0.1f, mFarPlane = 100.0f;
+    glm::mat4 mProj, mView;
+    purrTransform *mTransform = nullptr;
+  };
+
+}
+
+#endif
---
+++ core/include/PurrfectEngine/transform.hpp
@@ -0,0 +1,60 @@
+#ifndef PURRENGINE_TRANSFORM_HPP_
+#define PURRENGINE_TRANSFORM_HPP_
+
+#include <glm/glm.hpp>
+
+namespace PurrfectEngine {
+
+  class purrTransform {
+  public:
+    purrTransform(glm::vec3 position = glm::vec3(0.0f), glm::vec3 rotation = glm::vec3(0.0f), glm::vec3 scale = glm::vec3(1.0f)):
+      mPos(position), mRot(rotation), mScl(scale)
+    {}
+
+    ~purrTransform() {
+
+    }
+
+    const glm::mat4 getTransform() { 
+      return mTransform = glm::translate(glm::mat4(1.0f), mPos)
+                          * glm::rotate(glm::mat4(1.0f), glm::radians(mRot.x), glm::vec3(1.0f, 0.0f, 0.0f))
+                          * glm::rotate(glm::mat4(1.0f), glm::radians(mRot.y), glm::vec3(0.0f, 1.0f, 0.0f))
+                          * glm::rotate(glm::mat4(1.0f), glm::radians(mRot.z), glm::vec3(0.0f, 0.0f, 1.0f))
+                          * glm::scale(glm::mat4(1.0f), mScl);
+    }
+    
+    glm::vec3 getPosition() const { return mPos; }
+    glm::vec3 getRotation() const { return mRot; }
+    glm::vec3 getScale   () const { return mScl; }
+
+    void getPosition(glm::vec3 v) { mPos = v; }
+    void getRotation(glm::vec3 v) { mRot = v; }
+    void getScale   (glm::vec3 v) { mScl = v; }
+
+    const glm::vec3 getForward() const {
+      return glm::vec3(sin(mRot.y), -tan(mRot.x), cos(mRot.y));
+    }
+
+    const glm::vec3 getRight() const {
+      glm::vec3 forward = getForward();
+      forward = glm::normalize(forward);
+      glm::vec3 worldUp(0.0f, 1.0f, 0.0f);
+      
+      return glm::normalize(glm::cross(worldUp, forward));
+    }
+
+    const glm::vec3 getUp() const {
+      glm::vec3 forward = getForward();
+      forward = glm::normalize(forward);
+      glm::vec3 right = getRight();
+
+      return glm::normalize(glm::cross(forward, right));
+    }
+  private:
+    glm::mat4 mTransform;
+    glm::vec3 mPos, mRot, mScl;
+  };
+
+}
+
+#endif
